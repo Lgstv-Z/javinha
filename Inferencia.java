5276223a68b0b8658ebba1ff23ada6cf79cc508f
Lgstv-Z <lgstvs11@gmail.com>
2026-04-23 20:27:27 -0300

    Add Inferencia e PromocoesAutomaticas3

---
+++ Inferencia.java
@@ -0,0 +1,28 @@
+public class Inferencia {
+	public static void main(String[] args){
+		// Inferência - é o processo lógico de deduzir conclusões a partir de premissas, evidências ou pistas, indo além do que está explicitamente escrito.
+		/*
+			É uma interpretação baseada em fatos, indícios e conhecimento prévio, permitindo entender intenções implícitas em textos ou situações,
+			funcionando como uma ponte entre o que foi dito e o que se conclui.
+
+		*/
+		// Uso de var em Java
+		/* 
+			- Var foi introduziada no Java 10.
+			- Declarar uma variável.
+			- Inferência de valor.
+		*/
+
+		var numero = 10; // inferência int
+		var usuario = new Inferencia(); // inferência do tipo Usuario
+
+		// Limitações do Var
+		/*
+			- Variáveis locais.
+			- Valores nulos.
+		*/
+
+		System.out.println(numero);
+		System.out.println(usuario);
+	}
+}
---
+++ PromocoesAutomaticas3.java
@@ -0,0 +1,10 @@
+public class PromocoesAutomaticas3 {
+	public static void main(String[] args){
+		// Se um operando é double, o outro é promovido para double.
+		int a = 10;
+		double b = 1.5;
+		double c = a + b;
+
+		System.out.println(c);
+	}
+}
