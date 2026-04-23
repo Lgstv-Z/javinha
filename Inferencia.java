a2dd71b1bfdb42e8f3026091c0c883397d2762ec
Lgstv-Z <lgstvs11@gmail.com>
2026-04-23 20:48:16 -0300

    JavaDOC Inferencia.java

--- Inferencia.java
+++ Inferencia.java
@@ -1,11 +1,11 @@
 public class Inferencia {
-	public static void main(String[] args){
-		// Inferência - é o processo lógico de deduzir conclusões a partir de premissas, evidências ou pistas, indo além do que está explicitamente escrito.
-		/*
-			É uma interpretação baseada em fatos, indícios e conhecimento prévio, permitindo entender intenções implícitas em textos ou situações,
-			funcionando como uma ponte entre o que foi dito e o que se conclui.
 
+		/** 
+		 *  Inferência - é o processo lógico de deduzir conclusões a partir de premissas, evidências ou pistas, indo além do que está explicitamente escrito.
+		 * É uma interpretação baseada em fatos, indícios e conhecimento prévio, permitindo entender intenções implícitas em textos ou situações, funcionando como uma ponte entre o que foi dito e o que se conclui.	
 		*/
+
+	public static void main(String[] args){
 		// Uso de var em Java
 		/* 
 			- Var foi introduziada no Java 10.
@@ -13,7 +13,7 @@ public class Inferencia {
 			- Inferência de valor.
 		*/
 
-		var numero = 10; // inferência int
+		var numero = 10; // inferência int  
 		var usuario = new Inferencia(); // inferência do tipo Usuario
 
 		// Limitações do Var
